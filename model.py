206242799ddc9a849ab85f78c3355a871547dc10
Steve Krulewitz <skrulx@gmail.com>
2011-05-03 19:43:55 -0700

    Finish off model

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from google.appengine.ext import db
 
 class UserPreference(db.Model):
@@ -8,7 +10,7 @@ class UserPreference(db.Model):
 
     @classmethod
     def get_by_email(cls, email):
-        pass
+        return UserPreference.get_by_key_name(email)
 
     @classmethod
     def get_by_user(cls, user):
@@ -16,26 +18,50 @@ class UserPreference(db.Model):
         if up is None:
             up = UserPreference(key_name=user.email())
             up.user = user
-            up.last_update_week_id = None
-            db.put(up)
+            up.put()
         return up
 
     @classmethod
-    def get_all_user_preferences(cls):
-        pass
+    def get_all(cls):
+        return UserPreference.all().fetch(1000)
 
     @classmethod
-    def get_users_without_updates_for_week(cls, week_id):
-        pass
+    def get_all_without_updates_for_week(cls, week_id):
+        up_keys = {}
+        ups = UserPreference.all().fetch(1000)
+        for up in ups:
+            up_keys[str(up.key())] = up
+        wus = WeekUpdate.get_all_for_week_id(week_id)
+        for wu in wus:
+            del up_keys[str(wu.parent().key())]
+        return up_keys.values()
+
 
 class WeekUpdate(db.Model):
-    submit_date = db.DateProperty()
+    submit_date = db.DateTimeProperty(auto_now=True)
+    user_preference = db.ReferenceProperty(UserPreference)
+    week_id = db.StringProperty()
     body = db.StringProperty()
 
     @classmethod
-    def get_week_updates_for_week_id(cls, week_id):
-        pass
+    def current_week_id(cls):
+        return datetime.now().strftime('%Y%U')
 
     @classmethod
-    def add_week_update(cls, user, week_id, body):
-        pass
+    def get_all_for_week_id(cls, week_id):
+        return WeekUpdate.all().filter('week_id = ', week_id).fetch(1000)
+
+    @classmethod
+    def add(cls, up, week_id, body):
+        wu = WeekUpdate.get_by_key_name(week_id, parent=up)
+        if wu is None:
+            wu = WeekUpdate(key_name=week_id, parent=up)
+            wu.user_preference = up
+            wu.week_id = week_id
+
+        wu.body = body
+        wu.put()
+
+        up.last_update_week_id = week_id
+        up.put()
+        return wu
--- test_model.py
+++ test_model.py
@@ -16,11 +16,88 @@ class DemoTestCase(unittest.TestCase):
   def tearDown(self):
     self.testbed.deactivate()
 
+  def add_user_preference(self, email):
+      user = users.User(email=email)
+      up = model.UserPreference(key_name=user.email())
+      up.user = user
+      up.put()
+      return up
+
   def test_get_user_preference_by_user(self):
       email = 'homer@snpp.com'
       user = users.User(email=email)
-      user_preference = model.UserPreference.get_by_user(user)
-      self.assertEqual(email, user_preference.user.email())
+      up = model.UserPreference.get_by_user(user)
+      self.assertEqual(email, up.user.email())
 
-      user = model.UserPreference.get_by_key_name(email)
-      self.assertTrue(user is not None)
+      up = model.UserPreference.get_by_key_name(email)
+      self.assertEqual(email, up.user.email())
+
+  def test_get_by_email(self):
+      email = 'homer@snpp.com'
+      up = model.UserPreference.get_by_email(email)
+      self.assertTrue(up is None)
+
+      self.add_user_preference(email)
+
+      up = model.UserPreference.get_by_email(email)
+      self.assertEqual(email, up.user.email())
+
+  def test_get_all_user_preferences(self):
+      ups = model.UserPreference.get_all();
+      self.assertEqual(0, len(ups))
+
+      email = 'homer@snpp.com'
+      self.add_user_preference(email)
+
+      ups = model.UserPreference.get_all();
+      self.assertEqual(1, len(ups))
+      self.assertEqual(email, ups[0].user.email())
+
+  def test_add_week_update(self):
+      email = 'homer@snpp.com'
+      self.add_user_preference(email)
+      up = model.UserPreference.get_by_email(email)
+
+      week_id = '201101'
+      body = 'hello world'
+      uw = model.WeekUpdate.add(up, week_id, body)
+      self.assertEqual(week_id, uw.week_id)
+      self.assertEqual(up.user.email(), uw.user_preference.user.email())
+      self.assertEqual(body, uw.body)
+      self.assertEqual(up.last_update_week_id, week_id)
+
+      body = 'hello new world'
+      uw = model.WeekUpdate.add(up, week_id, body)
+      self.assertEqual(week_id, uw.week_id)
+      self.assertEqual(body, uw.body)
+      self.assertEqual(up.last_update_week_id, week_id)
+
+  def test_get_all_for_week_id(self):
+      week_id = '201101'
+      wus = model.WeekUpdate.get_all_for_week_id(week_id)
+      self.assertEqual(0, len(wus))
+      up1 = self.add_user_preference('homer@snpp.com')
+      up2 = self.add_user_preference('marge@snpp.com')
+      uw = model.WeekUpdate.add(up1, week_id, 'body1')
+      uw = model.WeekUpdate.add(up2, week_id, 'body2')
+
+      wus = model.WeekUpdate.get_all_for_week_id(week_id)
+      self.assertEqual(2, len(wus))
+      self.assertTrue(wus[0].body == 'body1' or wus[0].body == 'body2')
+      self.assertTrue(wus[1].body == 'body1' or wus[1].body == 'body2')
+
+  def test_get_all_without_updates_for_week(self):
+      week_id = '201101'
+      up1 = self.add_user_preference('homer@snpp.com')
+      up2 = self.add_user_preference('marge@snpp.com')
+
+      ups = model.UserPreference.get_all_without_updates_for_week(week_id)
+      self.assertEqual(2, len(ups))
+
+      uw = model.WeekUpdate.add(up1, week_id, 'body1')
+      ups = model.UserPreference.get_all_without_updates_for_week(week_id)
+      self.assertEqual(1, len(ups))
+
+      uw = model.WeekUpdate.add(up2, week_id, 'body2')
+      ups = model.UserPreference.get_all_without_updates_for_week(week_id)
+      self.assertEqual(0, len(ups))
